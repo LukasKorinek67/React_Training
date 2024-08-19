a6f6cd914c4a77ff84303471175b3f58fba15d9e
Lukáš Kořínek <lukas.korinek@tul.cz>
2024-08-19 17:04:20 +0200

    3rd block - filtering implemented

--- client/src/components/RecipesList.js
+++ client/src/components/RecipesList.js
@@ -1,8 +1,9 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useMemo} from 'react';
 import recipesData from '../mockup-data/recipes.json';
 import Navbar from "react-bootstrap/Navbar";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import ToggleButton from "react-bootstrap/ToggleButton";
+import Form from "react-bootstrap/Form";
 import RecipesBigGrid from "./RecipesBigGrid";
 import RecipesSmallGrid from "./RecipesSmallGrid";
 import RecipesTableList from "./RecipesTableList";
@@ -14,10 +15,22 @@ import { mdiGridLarge, mdiGrid, mdiTable } from '@mdi/js';
 export default function RecipesList() {
     const [recipes, setRecipes] = useState(null);
     const [viewType, setViewType] = useState('big-detail');
+    const [searchBy, setSearchBy] = useState("");
 
     useEffect(() => {
         getAllRecipes();
     }, []);
+
+    const filteredRecipes = useMemo(() => {
+        if (recipes != null) {
+            return recipes.filter((recipe) => {
+                return (
+                    recipe.name.toLocaleLowerCase().includes(searchBy.toLocaleLowerCase()) ||
+                    recipe.description.toLocaleLowerCase().includes(searchBy.toLocaleLowerCase())
+                );
+            });
+        }
+    }, [searchBy, recipes]);
 
     function getAllRecipes() {
         // tady by bylo volání na backend
@@ -31,11 +44,25 @@ export default function RecipesList() {
         { name: 'Tabulka', value: 'table', icon: mdiTable },
     ];
 
+    function handleSearchChange(event) {
+        setSearchBy(event.target.value);
+    }
+
     return (
         <>
             <Navbar bg="light" >
-                <Navbar.Brand className="ps-4">Seznam receptů</Navbar.Brand>
-                <ButtonGroup className="mb-4 ms-auto pe-4 pt-2">
+                <Navbar.Brand className="ps-4 mt-1">Seznam receptů</Navbar.Brand>
+                <Form className="d-flex ms-auto me-4 pt-0">
+                    <Form.Control
+                        id="searchInput"
+                        type="search"
+                        placeholder="Hledat"
+                        className="me-1"
+                        aria-label="Search"
+                        onChange={handleSearchChange}
+                    />
+                </Form>
+                <ButtonGroup className="pe-4">
                     {viewTypes.map((type) => (
                         <ToggleButton
                             key={type.value}
@@ -53,9 +80,9 @@ export default function RecipesList() {
                 </ButtonGroup>
             </Navbar>
 
-            {viewType === 'big-detail' && <RecipesBigGrid recipes={recipes}/>}
-            {viewType === 'small-detail' && <RecipesSmallGrid recipes={recipes}/>}
-            {viewType === 'table' && <RecipesTableList recipes={recipes} />}
+            {viewType === 'big-detail' && <RecipesBigGrid recipes={filteredRecipes}/>}
+            {viewType === 'small-detail' && <RecipesSmallGrid recipes={filteredRecipes}/>}
+            {viewType === 'table' && <RecipesTableList recipes={filteredRecipes} />}
         </>
     );
 }
