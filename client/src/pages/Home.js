1125c6c4a46ca9eddf05f484832aa02970a6e99f
Lukáš Kořínek <lukas.korinek@tul.cz>
2024-08-30 09:00:49 +0200

    deployment - removed background images on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,28 +12,32 @@ export default function Home() {
             <Carousel>
                 <Carousel.Item>
                     <Link to="/recipeList">
-                        <img
+                        <div className="d-block w-100" style={{ height: '600px', backgroundColor: 'var(--bs-info)' }}>
+                        {/*<img
                             className="d-block w-100"
                             src="carousel_recipes.png"
                             alt={strings.RECIPES_LIST}
                             style={{ height: '700px', objectFit: 'cover' }}
-                        />
+                        />*/}
+                        </div>
                         <Carousel.Caption>
-                            <h3>{strings.RECIPES_LIST}</h3>
+                            <h1>{strings.RECIPES_LIST}</h1>
                             <p>{strings.CAROUSEL_GO_TO_RECIPES}</p>
                         </Carousel.Caption>
                     </Link>
                 </Carousel.Item>
                 <Carousel.Item>
                     <Link to="/ingredientList">
-                        <img
+                        <div className="d-block w-100" style={{ height: '600px', backgroundColor: 'var(--bs-secondary)' }}>
+                        {/*<img
                             className="d-block w-100"
                             src="carousel_ingredients.png"
                             alt={strings.INGREDIENTS_LIST}
                             style={{ height: '700px', objectFit: 'cover' }}
-                        />
+                        />*/}
+                        </div>
                         <Carousel.Caption>
-                            <h3>{strings.INGREDIENTS_LIST}</h3>
+                            <h1>{strings.INGREDIENTS_LIST}</h1>
                             <p>{strings.CAROUSEL_GO_TO_INGREDIENTS}</p>
                         </Carousel.Caption>
                     </Link>
