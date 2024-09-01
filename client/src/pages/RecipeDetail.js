acd5c443d56d0ee179d32ba5ab233fc0df7c9213
Lukáš Kořínek <lukas.korinek@tul.cz>
2024-09-01 22:36:42 +0200

    deployment - recipeDetail page upgraded

--- client/src/pages/RecipeDetail.js
+++ client/src/pages/RecipeDetail.js
@@ -2,6 +2,14 @@ import { useLocation } from 'react-router-dom';
 import MainNavbar from "../components/MainNavbar";
 import React, {useEffect, useState} from "react";
 import requestHandler from "../services/RequestHandler";
+import Image from 'react-bootstrap/Image';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {useData} from "../context/DataProvider";
+import * as strings from "../text/strings";
+import Icon from "@mdi/react";
+import {mdiLoading} from "@mdi/js";
 
 
 export default function RecipeDetail() {
@@ -11,6 +19,7 @@ export default function RecipeDetail() {
     const [recipeLoadCall, setRecipeLoadCall] = useState({
         state: "pending",
     });
+    const { ingredientsLoadCall } = useData();
 
     useEffect(() => {
         requestHandler.getRecipe(id)
@@ -27,19 +36,63 @@ export default function RecipeDetail() {
             });;
     }, []);
 
+    const getIngredientName = (id) => {
+        const ingredient = ingredientsLoadCall.data.find(ingredient => ingredient.id === id);
+        return ingredient.name;
+    }
+
+    const showRecipe = () => {
+        if (recipeLoadCall.state === "success") {
+            return (
+                <>
+                    <h1 className="text-center fw-normal m-5">{recipeLoadCall.data.name}</h1>
+                    <p className="fw-light m-5">{recipeLoadCall.data.description}</p>
+                    {
+                        recipeLoadCall.data.imgUri.trim() !== "" &&
+                        <>
+                            <Container>
+                                <Row>
+                                    <Col className="text-center">
+                                        <Image src={recipeLoadCall.data.imgUri} alt={recipeLoadCall.data.name} fluid
+                                               rounded/>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </>
+                    }
+                    {
+                        (recipeLoadCall.data.ingredients && recipeLoadCall.data.ingredients.length > 0) &&
+                        <div className="m-5">
+                            <h2>{strings.INGREDIENTS}:</h2>
+                            <ul>
+                                {recipeLoadCall.data.ingredients.map((ingredient) => (
+                                    <li key={ingredient.id + ingredient.amount}>{getIngredientName(ingredient.id)}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    }
+                </>
+            );
+        } else if (recipeLoadCall.state === "error") {
+            return (
+                <div className="request_error">
+                    <h1>{strings.ERROR_LOAD_DATA}</h1>
+                    <pre>{JSON.stringify(recipeLoadCall.error, null, 2)}</pre>
+                </div>
+            );
+        } else if (recipeLoadCall.state === "pending") {
+            return (
+                <div className="loading_icon">
+                    <Icon size={2} path={mdiLoading} spin={true}/>
+                </div>
+            );
+        }
+    }
+
     return (
         <>
             <MainNavbar />
-            <h1>Detail receptu</h1>
-            <p>ID receptu: {id}</p>
-            {
-                recipeLoadCall.state === "success" &&
-                <>
-                    <p>{recipeLoadCall.data.name}</p>
-                    <p>{recipeLoadCall.data.description}</p>
-                </>
-            }
-            {recipeLoadCall.state === "error" && <p>ERROR: {recipeLoadCall.error}</p>}
+            {showRecipe()}
         </>
     );
 }
